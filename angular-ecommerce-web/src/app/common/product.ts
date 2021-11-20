7c9e19627876629f20fe299b66dd125ac2439d7f
hunchmen <valalberto0214@gmail.com>
2021-11-20 18:18:44 +0800

    get product list service

---
+++ angular-ecommerce-web/src/app/common/product.ts
@@ -0,0 +1,14 @@
+import { StringMap } from "@angular/compiler/src/compiler_facade_interface";
+
+export class Product {
+    sku!: string;
+    name!: string;
+    description!: string;
+    unitPrice!: number;
+    imageUrl!: string;
+    active!: boolean;
+    unitInStock!: number;
+    dateCreated!: Date;
+    lastUpdated!: Date;
+
+}
