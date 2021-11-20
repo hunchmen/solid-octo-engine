6e1087a573fe8e36694cde3ee8656e873e912265
hunchmen <valalberto0214@gmail.com>
2021-11-20 17:26:24 +0800

    add @CrossOrigin annotation in repository

--- src/main/java/com/via/ecommerce/app/dao/ProductCategoryRepository.java
+++ src/main/java/com/via/ecommerce/app/dao/ProductCategoryRepository.java
@@ -2,10 +2,12 @@ package com.via.ecommerce.app.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.via.ecommerce.app.entity.ProductCategory;
 
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@CrossOrigin
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long>{
-
+	
 }
--- src/main/java/com/via/ecommerce/app/dao/ProductRepository.java
+++ src/main/java/com/via/ecommerce/app/dao/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.via.ecommerce.app.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.via.ecommerce.app.entity.Product;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
 }
